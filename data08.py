9f50b1682f9cd74c56388ebb38b35b9e0ba1c92a
smurfhrk1 <smurfhrk1@gmail.com>
2026-09-04 14:52:02 +0700

    Update data08.py

--- data08.py
+++ data08.py
@@ -1,5 +1,4 @@
 contacts = [
-    {'nama': 'JOSHUA MAILI', 'email': 'joshua.maili@kemenimipas.go.id'},
     {'nama': 'JOSHUA MARSAHALA SIALLAGAN', 'email': 'joshua.marsahala.siallagan98@kemenimipas.go.id'},
     {'nama': 'JOSHUA SIMATUPANG', 'email': 'joshua.simatupang99@kemenimipas.go.id'},
     {'nama': 'JOSHUA SUHERMAN SIHOMBING', 'email': 'joshua.suherman@kemenimipas.go.id'},
